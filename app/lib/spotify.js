2e0cfc8a2bbd626cc5c9395b007b98cc1bf616e9
chrysanthemum <asingiser@gmail.com>
2023-10-13 12:29:16 -0400

    get token for requests

--- app/lib/spotify.js
+++ app/lib/spotify.js
@@ -1,8 +1,31 @@
 import SpotifyWebApi from 'spotify-web-api-js'
 
-const spotifyApi = new SpotifyWebApi({
-  clientId: process.env.SPOTIFY_CLIENT_ID,
-  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+const client_id = process.env.SPOTIFY_CLIENT_ID
+const client_secret = process.env.SPOTIFY_CLIENT_SECRET
+
+const authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
+
+request.post(authOptions, function(error, response, body) {
+  if(!error && response.statusCode === 200) {
+    const token = body.access_token
+    console.log(token)
+  }
 })
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: client_id,
+  clientSecret: client_secret,
+})
+
+spotifyApi.setAccessToken(token)
 
 export default spotifyApi
